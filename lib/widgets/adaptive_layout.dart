d19904f3e588e28097795491c0373fef83adfdc2
karim242 <karimelsa38@gmail.com>
2024-02-28 21:55:16 +0200

    add font style and images

---
+++ lib/widgets/adaptive_layout.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout(
+      {super.key,
+      required this.mobileLayout,
+      required this.tabletLayout,
+      required this.desketopLayout});
+  final WidgetBuilder mobileLayout, tabletLayout, desketopLayout;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth < 600) {
+          return mobileLayout(context);
+        } else if (constraints.maxWidth < 900) {
+          return tabletLayout(context);
+        } else {
+          return desketopLayout(context);
+        }
+      },
+    );
+  }
+}
